657a8c489eeb5595e39dbdd7e328b8e4d4c55fcb
regisfaria <rgisfaria@yahoo.com.br>
2021-06-15 21:38:58 -0300

    changes

---
+++ src/modules/items/infra/typeorm/entities/Item.ts
@@ -0,0 +1,70 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  JoinTable,
+} from 'typeorm';
+
+import { User } from '@modules/users/infra/typeorm/entities/User';
+
+@Entity('items')
+export class Item {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToMany(() => User, user => user.id)
+  @JoinTable({
+    name: 'user_items',
+    joinColumns: [{ name: 'userId' }],
+    inverseJoinColumns: [{ name: 'id' }],
+  })
+  user: User;
+
+  @Column('uuid')
+  userId: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  category: string;
+
+  @Column('int')
+  minimumStock?: number;
+
+  @Column('int')
+  daysToNotifyExpirationDate?: number;
+
+  @Column()
+  image?: string;
+
+  @Column('boolean')
+  active: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  // @Expose({ name: 'imageUrl' })
+  // getImageUrl(): string | null {
+  //   if (!this.image) {
+  //     return null;
+  //   }
+
+  //   switch (uploadConfig.driver) {
+  //     case 'disk':
+  //       return this.image
+  //         ? `${process.env.APP_API_URL}/files/${this.image}`
+  //         : null;
+  //     case 's3':
+  //       return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.image}`;
+  //     default:
+  //       return null;
+  //   }
+  // }
+}
---
+++ src/modules/stocks/infra/typeorm/entities/Stock.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  JoinTable,
+} from 'typeorm';
+
+import { Item } from '@modules/items/infra/typeorm/entities/Item';
+
+@Entity('stocks')
+export class Stock {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToMany(() => Item, item => item.id)
+  @JoinTable({
+    name: 'item_stocks',
+    joinColumns: [{ name: 'itemId' }],
+    inverseJoinColumns: [{ name: 'id' }],
+  })
+  item: Item;
+
+  @Column('uuid')
+  itemId: string;
+
+  @Column()
+  type: string;
+
+  @Column('int')
+  quantity: number;
+
+  @Column('decimal')
+  value: number;
+
+  @Column('timestamp')
+  expirationDate?: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

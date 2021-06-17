3b3c1e4e954bb6515c989164e1e9202dd1f676b0
regisfaria <rgisfaria@yahoo.com.br>
2021-06-17 19:10:03 -0300

    created auth middleware

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '@config/auth';
+import { AppError } from '@shared/errors/AppError';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  _response: Response,
+  next: NextFunction,
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: userId } = verify(token, authConfig.secretToken) as IPayload;
+
+    request.user = { id: userId };
+
+    next();
+  } catch (invalidToken) {
+    throw new AppError('Invalid Token or Refresh Token', 401);
+  }
+}

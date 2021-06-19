743d2e43639b8c4d0881e5a4eb57e6feebe85aa7
phdgamer <pedro380dalpa@gmail.com>
2021-06-19 16:28:16 -0300

    feat a stock output route

--- src/shared/infra/http/routes/stock.routes.ts
+++ src/shared/infra/http/routes/stock.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { InputStockController } from '@modules/stocks/useCases/InputStock/InputStockController';
+import { OutputStockController } from '@modules/stocks/useCases/outputStock/outputStockController';
 
 const stockRoutes = Router();
 
 const inputStockController = new InputStockController();
+const outputStockController = new OutputStockController();
 
 stockRoutes.post('/input', inputStockController.handle);
+stockRoutes.post('/output', outputStockController.handle);
 
 export { stockRoutes };

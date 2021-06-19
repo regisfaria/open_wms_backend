16a622941ea55327d16f969dab02c2132d7732c4
regisfaria <rgisfaria@yahoo.com.br>
2021-06-19 13:53:08 -0300

    added user tokens

---
+++ src/modules/users/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,47 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateUserTokenDTO } from '@modules/users/dtos/ICreateUserTokenDTO';
+import { IUsersTokensRepository } from '@modules/users/repositories/IUsersTokensRepository';
+
+import { UserToken } from '../entities/UserToken';
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserToken>;
+
+  constructor() {
+    this.repository = getRepository(UserToken);
+  }
+
+  async create(data: ICreateUserTokenDTO): Promise<UserToken> {
+    const token = this.repository.create(data);
+
+    await this.repository.save(token);
+
+    return token;
+  }
+
+  async findByUserIdAndToken(
+    userId: string,
+    token: string,
+  ): Promise<UserToken> {
+    const userToken = await this.repository.findOne({
+      where: { userId, token },
+    });
+
+    return userToken;
+  }
+
+  async findByToken(token: string): Promise<UserToken> {
+    const userToken = await this.repository.findOne({
+      where: { token },
+    });
+
+    return userToken;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}
+
+export { UsersTokensRepository };

dd727392df4012bf7dca0e5c6cd81e9490d06dea
regisfaria <rgisfaria@yahoo.com.br>
2021-06-19 13:54:33 -0300

    registered userTokens repository into containers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,11 +5,18 @@ import './providers';
 import { ItemsRepository } from '@modules/items/infra/typeorm/repositories/ItemsRepository';
 import { IItemsRepository } from '@modules/items/repositories/IItemsRepository';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/users/infra/typeorm/repositories/UsersTokensRepository';
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/users/repositories/IUsersTokensRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  'UsersTokensRepository',
+  UsersTokensRepository,
 );
 
 container.registerSingleton<IItemsRepository>(

a1ee504878b1dea058f0a602b5cb0d6fd1cc010c
regisfaria <rgisfaria@yahoo.com.br>
2021-06-19 16:52:07 -0300

    added a new endpoint

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { ConfirmUserController } from '@modules/users/useCases/confirmUser/ConfirmUserController';
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
 import { UpdateUserController } from '@modules/users/useCases/updateUser/UpdateUserController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
@@ -8,8 +9,10 @@ const userRoutes = Router();
 
 const createUserController = new CreateUserController();
 const updateUserController = new UpdateUserController();
+const confirmUserController = new ConfirmUserController();
 
 userRoutes.post('/', createUserController.handle);
 userRoutes.put('/:id', ensureAuthenticated, updateUserController.handle);
+userRoutes.patch('/verify/:token', confirmUserController.handle);
 
 export { userRoutes };

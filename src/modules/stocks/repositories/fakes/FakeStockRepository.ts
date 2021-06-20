8c0c19408cc110f0423bb0466282278e3c80ee7f
regisfaria <rgisfaria@yahoo.com.br>
2021-06-19 21:54:48 -0300

    commented since needs adjustments

--- src/modules/stocks/repositories/fakes/FakeStockRepository.ts
+++ src/modules/stocks/repositories/fakes/FakeStockRepository.ts
@@ -1,96 +1,96 @@
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 
-import { IStockRecordDTO } from '@modules/stocks/dtos/IStockRecordDTO';
-import { Stock } from '@modules/stocks/infra/typeorm/entities/Stock';
+// import { IStockRecordDTO } from '@modules/stocks/dtos/IStockRecordDTO';
+// import { Stock } from '@modules/stocks/infra/typeorm/entities/Stock';
 
-import { IStocksRepository } from '../IStocksRepository';
+// import { IStocksRepository } from '../IStocksRepository';
 
-class FakeStockRepository implements IStocksRepository {
-  private stocks: Stock[] = [];
+// class FakeStockRepository implements IStocksRepository {
+//   private stocks: Stock[] = [];
 
-  async sumInput(itemId: string): Promise<number> {
-    const allQuantity: number[] = [];
-    this.stocks.filter(stock => {
-      if (stock.itemId === itemId && stock.type === 'input') {
-        allQuantity.push(stock.quantity);
-      }
-      return stock;
-    });
+//   async sumInput(itemId: string): Promise<number> {
+//     const allQuantity: number[] = [];
+//     this.stocks.filter(stock => {
+//       if (stock.itemId === itemId && stock.type === 'input') {
+//         allQuantity.push(stock.quantity);
+//       }
+//       return stock;
+//     });
 
-    const quantity = allQuantity.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0,
-    );
+//     const quantity = allQuantity.reduce(
+//       (accumulator, currentValue) => accumulator + currentValue,
+//       0,
+//     );
 
-    return quantity;
-  }
+//     return quantity;
+//   }
 
-  async sumOutput(itemId: string): Promise<number> {
-    const allQuantity: number[] = [];
-    this.stocks.filter(stock => {
-      if (stock.itemId === itemId && stock.type === 'output') {
-        allQuantity.push(stock.quantity);
-      }
-      return stock;
-    });
+//   async sumOutput(itemId: string): Promise<number> {
+//     const allQuantity: number[] = [];
+//     this.stocks.filter(stock => {
+//       if (stock.itemId === itemId && stock.type === 'output') {
+//         allQuantity.push(stock.quantity);
+//       }
+//       return stock;
+//     });
 
-    const quantity = allQuantity.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0,
-    );
+//     const quantity = allQuantity.reduce(
+//       (accumulator, currentValue) => accumulator + currentValue,
+//       0,
+//     );
 
-    return quantity;
-  }
+//     return quantity;
+//   }
 
-  async input(data: IStockRecordDTO): Promise<Stock> {
-    const stock = new Stock();
+//   async input(data: IStockRecordDTO): Promise<Stock> {
+//     const stock = new Stock();
 
-    Object.assign(stock, { id: uuid(), ...data, type: 'input' });
+//     Object.assign(stock, { id: uuid(), ...data, type: 'input' });
 
-    this.stocks.push(stock);
+//     this.stocks.push(stock);
 
-    return stock;
-  }
+//     return stock;
+//   }
 
-  async output(data: IStockRecordDTO): Promise<Stock> {
-    const stock = new Stock();
+//   async output(data: IStockRecordDTO): Promise<Stock> {
+//     const stock = new Stock();
 
-    Object.assign(stock, { id: uuid(), ...data, type: 'output' });
+//     Object.assign(stock, { id: uuid(), ...data, type: 'output' });
 
-    this.stocks.push(stock);
+//     this.stocks.push(stock);
 
-    return stock;
-  }
+//     return stock;
+//   }
 
-  async sumBalance(itemId: string): Promise<number> {
-    const valueOutput: number[] = [];
-    this.stocks.filter(stock => {
-      if (stock.itemId === itemId && stock.type === 'output') {
-        valueOutput.push(stock.value);
-      }
-      return stock;
-    });
+//   async sumBalance(itemId: string): Promise<number> {
+//     const valueOutput: number[] = [];
+//     this.stocks.filter(stock => {
+//       if (stock.itemId === itemId && stock.type === 'output') {
+//         valueOutput.push(stock.value);
+//       }
+//       return stock;
+//     });
 
-    const sumValueOutput = valueOutput.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0,
-    );
+//     const sumValueOutput = valueOutput.reduce(
+//       (accumulator, currentValue) => accumulator + currentValue,
+//       0,
+//     );
 
-    const valueInput: number[] = [];
-    this.stocks.filter(stock => {
-      if (stock.itemId === itemId && stock.type === 'input') {
-        valueInput.push(stock.value);
-      }
-      return stock;
-    });
+//     const valueInput: number[] = [];
+//     this.stocks.filter(stock => {
+//       if (stock.itemId === itemId && stock.type === 'input') {
+//         valueInput.push(stock.value);
+//       }
+//       return stock;
+//     });
 
-    const sumValueInput = valueInput.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0,
-    );
+//     const sumValueInput = valueInput.reduce(
+//       (accumulator, currentValue) => accumulator + currentValue,
+//       0,
+//     );
 
-    return sumValueOutput - sumValueInput;
-  }
-}
+//     return sumValueOutput - sumValueInput;
+//   }
+// }
 
-export { FakeStockRepository };
+// export { FakeStockRepository };

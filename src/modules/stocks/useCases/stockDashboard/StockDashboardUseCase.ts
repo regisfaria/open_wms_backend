5ef67ba90ef88ced0f01bfa931e8920094536f81
regisfaria <rgisfaria@yahoo.com.br>
2021-06-19 21:54:00 -0300

    dashboard Stock use case

---
+++ src/modules/stocks/useCases/stockDashboard/StockDashboardUseCase.ts
@@ -0,0 +1,45 @@
+import { classToClass } from 'class-transformer';
+import { inject, injectable } from 'tsyringe';
+
+import { Item } from '@modules/items/infra/typeorm/entities/Item';
+import { IItemsRepository } from '@modules/items/repositories/IItemsRepository';
+import { IStocksRepository } from '@modules/stocks/repositories/IStocksRepository';
+
+interface IResponse {
+  item: Item;
+  totalQtd: number;
+  balance: number;
+}
+
+@injectable()
+class StockDashboardUseCase {
+  constructor(
+    @inject('StocksRepository')
+    private stocksRepository: IStocksRepository,
+
+    @inject('ItemsRepository')
+    private itemsRepository: IItemsRepository,
+  ) {}
+
+  async execute(userId: string): Promise<IResponse[]> {
+    const items = await this.itemsRepository.listAvailableAllFromUser({
+      userId,
+    });
+
+    const response: IResponse[] = [];
+
+    for (const item of items) {
+      const balance = await this.stocksRepository.sumBalance(item.id);
+
+      const totalQtd = await this.stocksRepository.sumTotalQtd(item.id);
+
+      item.imageUrl = `${process.env.APP_API_URL}/files/${item.image}`;
+
+      response.push({ balance, totalQtd, item });
+    }
+
+    return response;
+  }
+}
+
+export { StockDashboardUseCase };

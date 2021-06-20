da21f0be717ae509d3311c624fce11225cc53606
regisfaria <rgisfaria@yahoo.com.br>
2021-06-20 11:59:52 -0300

    created my cronjobs to notify users of their stocks

---
+++ src/shared/infra/http/cronjobs/ExpirationDateCronjob.ts
@@ -0,0 +1,91 @@
+import cron from 'node-cron';
+import { inject, injectable } from 'tsyringe';
+
+import { IItemsRepository } from '@modules/items/repositories/IItemsRepository';
+import { IStocksRepository } from '@modules/stocks/repositories/IStocksRepository';
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
+import { getDifferenceInDaysBetweenDates } from '@shared/utils/getDifferenceInDaysBetweenDates';
+import { resolveTemplatePath } from '@shared/utils/resolveTemplatePath';
+
+@injectable()
+class ExpirationDateCronjob {
+  constructor(
+    @inject('ItemsRepository')
+    private itemsRepository: IItemsRepository,
+
+    @inject('StocksRepository')
+    private stocksRepository: IStocksRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  async notifyUser(
+    userId: string,
+    itemName: string,
+    expirationDate: string,
+  ): Promise<void> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user || !user.verified) {
+      return;
+    }
+
+    const file = resolveTemplatePath('itemNearExpiration');
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: `[OpenWMS] Aviso de produto perto de vencimento`,
+      templateData: {
+        file,
+        variables: {
+          itemName,
+          expirationDate,
+        },
+      },
+    });
+  }
+
+  async execute(): Promise<void> {
+    const allItems = await this.itemsRepository.findAll();
+
+    allItems.forEach(async item => {
+      if (item.daysToNotifyExpirationDate >= 0) {
+        const stocks = await this.stocksRepository.findAllByItemId(item.id);
+
+        for (const stock of stocks) {
+          if (stock.expirationDate) {
+            const daysToExpiration = getDifferenceInDaysBetweenDates({
+              date: stock.expirationDate,
+            });
+
+            if (daysToExpiration <= item.daysToNotifyExpirationDate) {
+              await this.notifyUser(
+                item.userId,
+                item.name,
+                stock.expirationDate.toLocaleDateString('pt-br'),
+              );
+            }
+          }
+        }
+      }
+    });
+  }
+
+  runJob(): void {
+    console.info('[EXPIRATION DATE] Cronjob running...');
+
+    cron.schedule(`0 */${process.env.CRONJOB_MINUTES} * * * *`, async () => {
+      await this.execute();
+    });
+  }
+}
+
+export { ExpirationDateCronjob };
---
+++ src/shared/infra/http/cronjobs/MinimumStockCronjob.ts
@@ -0,0 +1,78 @@
+import cron from 'node-cron';
+import { inject, injectable } from 'tsyringe';
+
+import { IItemsRepository } from '@modules/items/repositories/IItemsRepository';
+import { IStocksRepository } from '@modules/stocks/repositories/IStocksRepository';
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
+import { resolveTemplatePath } from '@shared/utils/resolveTemplatePath';
+
+@injectable()
+class MinimumStockCronjob {
+  constructor(
+    @inject('ItemsRepository')
+    private itemsRepository: IItemsRepository,
+
+    @inject('StocksRepository')
+    private stocksRepository: IStocksRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  async notifyUser(
+    userId: string,
+    itemName: string,
+    currentQtd: number,
+  ): Promise<void> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user || !user.verified) {
+      return;
+    }
+
+    const file = resolveTemplatePath('minimumStock');
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: `[OpenWMS] Aviso de estoque baixo`,
+      templateData: {
+        file,
+        variables: {
+          itemName,
+          currentQtd,
+        },
+      },
+    });
+  }
+
+  async execute(): Promise<void> {
+    const allItems = await this.itemsRepository.findAll();
+
+    allItems.forEach(async item => {
+      if (item.minimumStock >= 0) {
+        const totalQtd = await this.stocksRepository.sumTotalQtd(item.id);
+
+        if (totalQtd <= item.minimumStock) {
+          await this.notifyUser(item.userId, item.name, totalQtd);
+        }
+      }
+    });
+  }
+
+  runJob(): void {
+    console.info('[MINIMUM STOCK] Cronjob running...');
+
+    cron.schedule(`0 */${process.env.CRONJOB_MINUTES} * * * *`, async () => {
+      await this.execute();
+    });
+  }
+}
+
+export { MinimumStockCronjob };

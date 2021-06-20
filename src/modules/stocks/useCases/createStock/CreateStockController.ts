227aa2471abda35d7236496a1f8ff0ecb473521e
regisfaria <rgisfaria@yahoo.com.br>
2021-06-19 21:53:21 -0300

    refactored into one useCase

---
+++ src/modules/stocks/useCases/createStock/CreateStockController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateStockUseCase } from './CreateStockUseCase';
+
+class CreateStockController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { type } = request.params;
+    const { itemId, quantity, value, expirationDate } = request.body;
+
+    const createStockUseCase = container.resolve(CreateStockUseCase);
+
+    const stock = await createStockUseCase.execute({
+      itemId,
+      quantity,
+      value,
+      expirationDate,
+      type,
+    });
+
+    return response.status(201).json(stock);
+  }
+}
+
+export { CreateStockController };

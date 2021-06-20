b8235742a567c0b2a5e5440ba1a3315aaa2de798
regisfaria <rgisfaria@yahoo.com.br>
2021-06-20 16:18:38 -0300

    new feature to list stocks

--- src/shared/infra/http/routes/stock.routes.ts
+++ src/shared/infra/http/routes/stock.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateStockController } from '@modules/stocks/useCases/createStock/CreateStockController';
+import { ListStocksController } from '@modules/stocks/useCases/listStocks/ListStocksController';
 import { StockDashboardController } from '@modules/stocks/useCases/stockDashboard/StockDashboardController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -9,11 +10,14 @@ const stockRoutes = Router();
 
 const createStockController = new CreateStockController();
 const dashboardStockController = new StockDashboardController();
+const listStocksController = new ListStocksController();
 
 stockRoutes.use(ensureAuthenticated);
 
 stockRoutes.post('/:type', createStockController.handle);
 
 stockRoutes.get('/dashboard', dashboardStockController.handle);
+
+stockRoutes.get('/byItem/:itemId', listStocksController.handle);
 
 export { stockRoutes };

c850afbd602b28d47b6f76df157d1d245032c9e8
regisfaria <rgisfaria@yahoo.com.br>
2021-06-20 15:48:50 -0300

    added a test driver for mailProvider when testing

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -2,6 +2,7 @@ import { container } from 'tsyringe';
 
 import { mailConfig } from '@config/mail';
 
+import { FakeMailProvider } from './fakes/FakeMailProvider';
 import { EtherealMailProvider } from './implementations/EtherealMailProvider';
 import { SESMailProvider } from './implementations/SESMailProvider';
 import { IMailProvider } from './models/IMailProvider';
@@ -9,6 +10,7 @@ import { IMailProvider } from './models/IMailProvider';
 const providers = {
   ethereal: container.resolve(EtherealMailProvider),
   ses: container.resolve(SESMailProvider),
+  test: new FakeMailProvider(),
 };
 
 container.registerInstance<IMailProvider>(
